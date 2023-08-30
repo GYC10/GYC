ba48c911f0736eac43033821e4172ccd76b7b849
eun hyang <ssm04090@gmail.com>
2023-08-30 23:12:13 +0900

    페이지 화면

--- src/App.js
+++ src/App.js
@@ -10,18 +10,17 @@ function App() {
     return (
         <div>
             <Router>
-
                 <div className="container">
                     <Routes>
-                        <Route path="/" exact component={ListUserComponent}></Route>
-                        <Route path="/users" component={ListUserComponent}></Route>
-                        <Route path="/add-users/:id" component={CreateUserComponent}></Route>
-                        <Route path="/view-user/:id" component={ViewUserComponent}></Route>
+                        <Route path="/" exact element={<ListUserComponent />}></Route>
+                        <Route path="/users" element={<ListUserComponent />}></Route>
+                        <Route path="/add-users/:id" element={<CreateUserComponent />}></Route>
+                        <Route path="/view-user/:id" element={<ViewUserComponent />}></Route>
                     </Routes>
                 </div>
             </Router>
 
-        </div>
+       </div>
     );
 }
 export default App;
